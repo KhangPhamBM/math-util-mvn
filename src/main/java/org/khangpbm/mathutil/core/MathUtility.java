031f99b85377dbe30ef621f732eccb20d167c3b0
PHAMKHANG <phamkhang12378@gmail.com>
2023-06-28 11:36:14 +0700

    Add screenshots and Update source using Recursion to see green forever

--- src/main/java/org/khangpbm/mathutil/core/MathUtility.java
+++ src/main/java/org/khangpbm/mathutil/core/MathUtility.java
@@ -10,12 +10,21 @@ public class MathUtility {
     public static final double PI = 3.1415;
     // we design this function: f(0) = 1, f(nev) throws exception
     //21! can't be handled with long => n>=21! throw exception
+//    public static long getFactorial(int n){
+//       if(n < 0 || n > 20) 
+//           throw new IllegalArgumentException("Invalid n. n must be between 0 to 20. Please!");
+//        long ans = 1;
+//        for(int i = 2; i <= n; i++)
+//            ans *= i;
+//        return ans;
+//    }
+    
     public static long getFactorial(int n){
        if(n < 0 || n > 20) 
            throw new IllegalArgumentException("Invalid n. n must be between 0 to 20. Please!");
-        long ans = 1;
-        for(int i = 2; i <= n; i++)
-            ans *= i;
-        return ans;
+        if(n < 2) return 1;
+        return getFactorial(n - 1) * n;
     }
 }
+
+//Tính
